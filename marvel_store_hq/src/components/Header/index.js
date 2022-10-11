7f587c1151ef0c4704ce6626b7b28ef6cb457bea
Murilo Bovati <murilo_bovati@hotmail.com>
2022-10-10 21:48:13 -0300

    Update PNG
    Adicionando as imagens que serão usadas no projeto

---
+++ marvel_store_hq/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom'
+
+const Header = () => {
+    return(
+        <header>
+            <nav>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
