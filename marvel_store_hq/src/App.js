a9c2c74244c59c2617d44eb62338afc588108022
Murilo Bovati <murilo_bovati@hotmail.com>
2022-10-13 01:01:50 -0300

    Upgrade Build

--- marvel_store_hq/src/App.js
+++ marvel_store_hq/src/App.js
@@ -1,13 +1,22 @@
 import GlobalStyle from './globalStyle';
 import Header from './components/Header';
-import Routers from './Routers';
+import {Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Quadrinho from './pages/Quadrinho'
+import Carrinho from './pages/Carrinho'
+import Contato from './pages/Contato'
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle/>
       <Header/>
-      <Routers/>
+      <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/Quadrinho' element={<Quadrinho/>}/>
+            <Route path='/Carrinho' element={<Carrinho/>}/>
+            <Route path='/Contato' element={<Contato/>}/>
+      </Routes>
     </div>
   );
 }
